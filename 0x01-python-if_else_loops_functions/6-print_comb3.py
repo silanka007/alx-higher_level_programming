fbf69603692d550328907d77a3e6e113cf2ca542
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-06-28 18:07:35 +0100

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for i in range(0, 10):
     for j in range(i, 10):
         if i != j:
-            if j != 9:
+            if i != 8:
                 print("{}{}".format(i, j), end=", ")
-            if i == 8:
+            else:
                 print("{}{}".format(i, j))
