58a97dbed1d0aedb4d3e55ccf223cc22b34b49d2
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-06-28 19:04:09 +0100

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    result = ""
+    for i in str:
+        ascii_val = ord(i)
+        if ascii_val in range(97, 123):
+            result += chr(ascii_val - 32)
+        else:
+            result += i
+    return result
