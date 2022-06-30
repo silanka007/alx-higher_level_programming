e8a0744fe4146b9dbc5dd43ad2bd824ac413fa5d
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-06-30 20:55:37 +0100

    code refactoring

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,8 @@ import hidden_4
 
 if __name__ == "__main__":
     names = dir(hidden_4)
-    pattern = re.compile("^\_\_[a-zA-Z0-9]{0,}\_\_$")
-    
+    pattern = re.compile("^__[a-zA-Z0-9]{0,}__$")
+
     for name in names:
         if not pattern.match(name):
             print(name)
