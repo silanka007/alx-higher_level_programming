6f468a2809fcc9fdd1c85a1af266d188d5ccdef6
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-06-30 22:24:29 +0100

    code refactoring

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
+from calculator_1 import add, sub
+
 
 def magic_calculation(a, b):
     if a < b:
@@ -9,3 +10,4 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
+    return
