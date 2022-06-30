f0927d68d96162599b5db6884fc44bb732a048dc
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-06-30 21:51:38 +0100

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) - 1 < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operator = args[2]
+    if operator not in ["+", "-", "*", "/"]:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(args[1])
+    b = int(args[3])
+
+    result = None
+    if operator == "+":
+        result = a + b
+    elif operator == "-":
+        result = a - b
+    elif operator == "*":
+        result = a * b
+    elif operator == "/":
+        result = a / b
+    if result:
+        print("{} {} {} = {}".format(a, operator, b, result))
