05de120d5394680a12943ed6fc322e6cf7b7351f
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-06-30 18:00:45 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    arg_len = len(args) - 1
+    print("{} {}{}".format(arg_len, "argument" if arg_len == 1 else "arguments", ":" if arg_len > 0 else "."))
+
+    if arg_len > 0:
+        for i in range(len(args)):
+            if i != 0:
+                print("{}: {}".format(i, args[i]))
