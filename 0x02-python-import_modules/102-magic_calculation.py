8876e4acab1e7dd9e52aed1bc9551cc66c9d77d5
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-06-30 22:29:08 +0100

    final solution

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from calculator_1 import add, sub
 
 
@@ -5,7 +6,7 @@ def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
-            c += i
+            c = add(c, i)
         return c
     else:
         return sub(a, b)
