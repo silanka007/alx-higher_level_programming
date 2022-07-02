18be222edce3872d3ce0094b1cfee23af45132b7
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-02 11:35:55 +0100

    code refactoring

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,5 +7,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         first_val = tuple_a[i] if i <= len(tuple_a) - 1 else 0
         second_val = tuple_b[i] if i <= len(tuple_b) - 1 else 0
         res.append(first_val + second_val)
-        
+
     return tuple(res)
