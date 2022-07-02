a1701e65dcebc2479eb0c85c46e568fba1f15adb
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-02 10:07:14 +0000

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix == None:
+        return None
+    for layer in matrix:
+        for i in range(len(layer)):
+            if i != len(layer) - 1:
+                print("{:d}".format(layer[i]), end=" ")
+            else:
+                print("{:d}".format(layer[i]))
