b1e487b7a61b85887bee3f58ef5cd7206fcf0c59
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-02 11:12:07 +0100

    consideration of empty items in matrices

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,11 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return None
     for layer in matrix:
-        for i in range(len(layer)):
-            if i != len(layer) - 1:
-                print("{:d}".format(layer[i]), end=" ")
-            else:
-                print("{:d}".format(layer[i]))
+        if layer is None or len(layer) == 0:
+            print()
+        else:
+            for i in range(len(layer)):
+                if i != len(layer) - 1:
+                    print("{:d}".format(layer[i]), end=" ")
+                else:
+                    print("{:d}".format(layer[i]))
