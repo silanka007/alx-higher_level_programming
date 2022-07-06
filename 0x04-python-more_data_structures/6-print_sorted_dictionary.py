9250bb0b3434fd80a39ca0af0dceabdbba1da4b2
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-06 18:33:00 +0100

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(list(a_dictionary))
+
+    for key in sorted_keys:
+        print(f"{key}: {a_dictionary[key]}")
