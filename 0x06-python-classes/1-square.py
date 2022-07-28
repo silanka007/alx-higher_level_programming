838b05ef9af55c4663ed3c20258aedf8bfcf219c
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-28 21:56:08 +0100

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,4 +6,4 @@ class Square:
     """ represents a square class with instance private attribute - size"""
 
     def __init__(self, size):
-        self._size = size
+        self._Square__size = size
