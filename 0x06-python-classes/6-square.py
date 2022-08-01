249809d7f82af0890685345afdcb72c522574d5c
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-08-01 20:38:21 +0100

    6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" This module contains the square class"""
+
+
+class Square:
+    """ represents a square class with instance private attribute - size"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        if (type(value) != int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self._Square__size = value
+
+    @property
+    def position(self):
+        return __position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len(value) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        x, y = value
+        not_pos_ints = x < 0 and y < 0
+        if (not isinstance(x, int) or not isinstance(y, int) or not_pos_ints):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = (x, y)
+
+    def area(self):
+        return self._Square__size ** 2
+
+    def my_print(self):
+        if self._Square__size == 0:
+            print("")
+        else:
+            i, j = 0, 0
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size):
+                print("{}{}".format(
+                    " " * self.__position[0], "#" * self.__size))
