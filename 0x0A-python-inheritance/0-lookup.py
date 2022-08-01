3b8e8b483db08a2a1d139253d5dbbcde61480220
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-08-01 22:45:38 +0100

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+Contains definition of the function lookup
+"""
+
 
 def lookup(obj):
+    """
+      returns a list of all available methods
+      and attributes of an object
+
+      @obj: an object
+    """
     return dir(obj)
