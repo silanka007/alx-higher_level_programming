7867c7c7175cafeb6034121722cc8009b0deee9d
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-08-03 07:53:04 +0100

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module contains the BaseGeometry class"""
+
+
+class BaseGeometry:
+    """ an empty class"""
+
+    def area(self):
+        """ raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if(not isinstance(value, int)):
+            raise TypeError(f"{name} must be an integer")
+        if (value <= 0):
+            raise ValueError(f"{name} must be greater than 0")
